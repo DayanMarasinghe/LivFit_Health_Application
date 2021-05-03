3493945d4aa3df3b6828a5a5f98770b369b2010d
DayanMarasinghe <dayanmarasinghe@gmail.com>
2021-05-04 02:32:56 +0530

    Workout retrieve commited

--- app/src/main/java/com/example/LivFit/CardioWorkout.java
+++ app/src/main/java/com/example/LivFit/CardioWorkout.java
@@ -7,13 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class CardioWorkout extends AppCompatActivity {
+
+
+
     private Button btn1;
     private Button btn2;
     private Button btn3;
     private Button btn4;
     private Button btn5;
     private Button btn6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,48 +32,51 @@ public class CardioWorkout extends AppCompatActivity {
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDynamicWorkout();
+                openDynamicWorkout("w6");
             }
         });
         btn5=(Button)findViewById(R.id.crunches);
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDynamicWorkout();
+                openDynamicWorkout("w2");
             }
         });
         btn4=(Button)findViewById(R.id.plank);
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDynamicWorkout();
+                openDynamicWorkout("w3");
             }
         });
         btn3=(Button)findViewById(R.id.jumpingjackbtn);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDynamicWorkout();
+                openDynamicWorkout("w4");
             }
         });
         btn2=(Button)findViewById(R.id.hykneebtn);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDynamicWorkout();
+                openDynamicWorkout("w5");
             }
         });
         btn1=(Button)findViewById(R.id.pshups);
         btn1.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
-                openDynamicWorkout();
+                openDynamicWorkout("w1");
             }
         });
     }
 
-    public void openDynamicWorkout(){
+    public void openDynamicWorkout(String wtypy){
         Intent intent = new Intent(this, PushupWorkout.class);
+        intent.putExtra("workout",wtypy);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/LivFit/PushupWorkout.java
+++ app/src/main/java/com/example/LivFit/PushupWorkout.java
@@ -1,14 +1,85 @@
 package com.example.LivFit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class PushupWorkout extends AppCompatActivity {
 
+    private Button confirm;
+    TextView a,b,burntnum;
+    DatabaseReference reff;
+    private EditText duration;
+    private int calburn;
+
+    String wtype;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pushup_workout);
+
+        a=(TextView)findViewById(R.id.textViewpshups);
+        b=(TextView)findViewById(R.id.textViewcalpmin);
+
+        Intent intent = getIntent();
+        wtype=intent.getStringExtra("workout");
+        reff=FirebaseDatabase.getInstance().getReference().child("Workout").child(wtype);
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String name = snapshot.child("name").getValue().toString();
+                String calburnt = snapshot.child("calburnt").getValue().toString();
+                a.setText(name);
+                b.setText(calburnt);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        duration =(EditText)findViewById(R.id.editTextdurationw);
+        burntnum=(TextView)findViewById(R.id.textViewbrntnum);
+
+        confirm =(Button) findViewById(R.id.dinbtn);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               calburn = calculateCalBurnt(duration,b);
+               String getcalburn = String.valueOf(calburn);
+
+              getcalburn = duration.getText().toString();
+               burntnum.setText(getcalburn);
+
+
+
+
+            }
+        });
+
+    }
+
+    public int calculateCalBurnt(EditText duration, TextView b){
+
+        int cvduration = Integer.parseInt(duration.getText().toString());
+        int cvcalburn = Integer.parseInt(b.getText().toString());
+
+        return cvcalburn*cvduration;
     }
 }
